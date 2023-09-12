4ce4ff866758bdcc1e55f544c31ef8796a2953b5
ShoreAgents <dev.shoreagents@gmail.com>
2023-09-12 09:31:53 +0800

    first deployment

---
+++ routes/index.js
@@ -0,0 +1,56 @@
+// 1. Import necessary modules and libraries
+import { OpenAI } from 'langchain/llms/openai';
+import { RetrievalQAChain } from 'langchain/chains';
+import { HNSWLib } from 'langchain/vectorstores/hnswlib';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+import express from "express";
+const router = express.Router()
+
+dotenv.config();
+
+const txtFilename = "gptdata";
+const txtPath = `./${txtFilename}.txt`;
+const VECTOR_STORE_PATH = `${txtFilename}.index`;
+
+router.get('/', async (req,res) =>{
+  res.render('index', { response:"Hi! How may I help you?" })
+})
+router.post('/', async (req,res) =>{
+  const question = req.body.userinput;
+  const output = await runWithEmbeddings(question);
+  console.log(output);
+  res.render('index', { response: output })
+})
+
+export const runWithEmbeddings = async (question) => {
+  const model = new OpenAI({temperature:0.7, modelName:"gpt-4"});
+
+  let vectorStore;
+  if (fs.existsSync(VECTOR_STORE_PATH)) {
+    vectorStore = await HNSWLib.load(VECTOR_STORE_PATH, new OpenAIEmbeddings());
+  } else {
+    const text = fs.readFileSync(txtPath, 'utf8');
+    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+    const docs = await textSplitter.createDocuments([text]);
+    vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+    await vectorStore.save(VECTOR_STORE_PATH);
+  }
+
+  
+
+
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+  
+  const res = await chain.call({
+    query: question,
+  });
+
+  const output = res.text;
+  return output;
+};
+
+export default router;
---
+++ server.js
@@ -0,0 +1,25 @@
+import expressLayouts from 'express-ejs-layouts';
+import express from 'express';
+import indexRouter from './routes/index.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
+
+
+
